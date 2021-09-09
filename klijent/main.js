d4403e3e2f3fb6dc23e2c472302ae9947366e26c
ivana-211 <ivana.grujic123@gmail.com>
2021-09-09 09:31:43 +0200

    updated

--- klijent/main.js
+++ klijent/main.js
@@ -2,19 +2,7 @@ import { Nedelja } from "./nedelja.js";
 import { Dan } from "./dan.js";
 import { Vezba } from "./vezba.js";
 
-fetch("https://localhost:5001/Vezbanje/PreuzmiNedelje").then(p =>
-{
-    p.json().then(data =>
-        {
-            data.forEach(nedelja =>
-                {
-                    const nedelja1 = new Nedelja("Trenutna nedelja", nedelja.id);
-                    nedelja1.crtaj(document.body);
-                });
-        });
-})
-
-/*const nedelja = new Nedelja("Sledeca nedelja", nedelja.id);
+/* const nedelja = new Nedelja("Prva nedelja", nedelja.id);
 nedelja.daniUNedelji[0].dodajVezbu("cucnjevi", "zuta");
 nedelja.daniUNedelji[0].dodajVezbu("sklekovi", "siva");
 nedelja.daniUNedelji[1].dodajVezbu("trcanje", "zelena");
@@ -26,4 +14,16 @@ nedelja.daniUNedelji[3].dodajVezbu("daska", "plava");
 nedelja.daniUNedelji[4].dodajVezbu("trcanje", "zelena");
 nedelja.daniUNedelji[5].dodajVezbu("dan za odmor", "bela");
 nedelja.daniUNedelji[6].dodajVezbu("dan za odmor", "bela");
-nedelja.crtaj(document.body);*/
+nedelja.crtaj(document.body); */
+
+fetch("https://localhost:5001/Vezbanje/PreuzmiNedelje").then(p =>
+{
+    p.json().then(data =>
+        {
+            data.forEach(nedelja =>
+                {
+                    const nedelja1 = new Nedelja("Trenutna nedelja", nedelja.id);
+                    nedelja1.crtaj(document.body);
+                });
+        });
+})
